468bbb5188d4c78d6ea858d54418476da9c5045f
LevaniIlashvili <levaniko900@gmail.com>
2024-02-19 17:02:44 +0400

    fetch friendships

--- db/queries/friendship.ts
+++ db/queries/friendship.ts
@@ -5,3 +5,10 @@ export const fetchFriendRequests = (userId: string) => {
     "user1"
   );
 };
+
+export const fetchFriendships = (userId: string) => {
+  return Friendship.find({
+    $or: [{ user1: userId }, { user2: userId }],
+    status: "accepted",
+  }).populate("user1 user2");
+};
