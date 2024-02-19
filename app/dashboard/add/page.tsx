605f3c7aca430f37863b68090c3960c42db7b7df
LevaniIlashvili <levaniko900@gmail.com>
2024-02-19 12:24:38 +0400

    functionality to add friend

--- app/dashboard/add/page.tsx
+++ app/dashboard/add/page.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+"use client";
+import { Button, Input } from "@nextui-org/react";
+import * as actions from "@/actions";
+import { useFormState } from "react-dom";
 
 const AddFriendPage = () => {
-  return <div>AddFriendPage</div>;
+  const [formState, action] = useFormState(actions.sendFriendRequest, {
+    errors: {},
+  });
+
+  return (
+    <main className="pt-32 pl-14">
+      <h1 className="text-5xl font-bold mb-6">Add a Friend</h1>
+      <form action={action} className="flex items-start gap-2">
+        <Input
+          name="email"
+          placeholder="your@example.com"
+          size="sm"
+          variant="bordered"
+          isInvalid={
+            !!formState.errors?.email?.length ||
+            !!formState.errors?._form?.length
+          }
+          errorMessage={
+            formState.errors?.email?.join(", ") ||
+            formState.errors?._form?.join(", ") ||
+            ""
+          }
+        />
+        <Button variant="solid" radius="sm" className="h-11" type="submit">
+          Add
+        </Button>
+      </form>
+    </main>
+  );
 };
 
 export default AddFriendPage;
