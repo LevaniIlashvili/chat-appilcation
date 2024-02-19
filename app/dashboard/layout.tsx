26f71f91b77128842fc681ebf33201db7735079c
LevaniIlashvili <levaniko900@gmail.com>
2024-02-19 17:01:15 +0400

    display friends

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,8 +1,18 @@
+import { authOptions } from "@/authOptions";
+import { fetchFriendships } from "@/db/queries/friendship";
 import { Button } from "@nextui-org/react";
+import { getServerSession } from "next-auth";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await getServerSession(authOptions);
+
+  // if (!session) redirect("/login");
+
+  const friendships = await fetchFriendships(session.user.id);
+
   return (
     <main className="w-full h-screen flex">
       <aside className="w-80 border py-4  px-6">
@@ -15,6 +25,24 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             className="mb-10"
           />
         </Link>
+        {friendships.length ? (
+          <div className="mb-4 font-medium">
+            <p className="text-sm font-medium text-gray-400 mb-4">Your Chats</p>
+            <ul>
+              {friendships.map((friendship) => {
+                return (
+                  <li key={friendship.id}>
+                    <Button variant="light" className="text-md font-medium">
+                      {friendship.user1.id === session.user.id
+                        ? friendship.user2.username
+                        : friendship.user1.username}
+                    </Button>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        ) : null}
         <div className="flex flex-col gap-4">
           <p className="text-sm font-medium text-gray-400">overview</p>
           <Button
