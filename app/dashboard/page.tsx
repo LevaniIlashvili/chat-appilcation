0ed05a9c436da7ebbb27d17effe7f4641441059c
LevaniIlashvili <levaniko900@gmail.com>
2024-02-19 17:01:56 +0400

    dummy content

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+import { signOut } from "next-auth/react";
+
 const DashboardPage = () => {
-  return <main>page</main>;
+  let chats = [];
+
+  return (
+    <main className="pt-32 pl-14">
+      <h1 className="text-5xl font-bold mb-6">Recent Chats</h1>
+      {chats.length === 0 && <p>No chats yet...</p>}
+      <button onClick={() => signOut()}>Sign out</button>
+    </main>
+  );
 };
 
 export default DashboardPage;
