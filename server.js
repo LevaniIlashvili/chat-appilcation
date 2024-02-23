2766aad5f78f8ba4b54904e622d77efd71efecbd
LevaniIlashvili <levaniko900@gmail.com>
2024-02-23 12:53:06 +0400

    handle and send events for friend requests

--- server.js
+++ server.js
@@ -15,15 +15,46 @@ const io = new Server(httpServer, {
 
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
+
+  const sessionId = socket.request._query["sessionId"];
+
+  if (sessionId) {
+    socket.join(`user_${sessionId}`);
+  }
+
   socket.on("join_room", (roomId) => {
     socket.join(roomId);
-    console.log(`user with id-${socket.id} joined room - ${roomId}`);
   });
 
   socket.on("send_msg", (data) => {
-    console.log(data, "DATA");
+    console.log(data, "message received from client");
     //This will send a message to a specific room ID
     socket.to(data.roomId).emit("receive_msg", data);
+  });
+
+  socket.on("send_friend_request", (data) => {
+    console.log(data);
+    console.log(
+      "joining room",
+      `user_${data.user2._id} to send friend request`
+    );
+    socket.to(`user_${data.user2._id}`).emit("receive_friend_request", data);
+  });
+
+  socket.on("respond_to_friend_request", (data) => {
+    console.log(data);
+    console.log(
+      "joining room",
+      `user_${data.user1._id} to respond to friend request`
+    );
+    if (data.status === "accept") {
+      socket
+        .to(`user_${data.user1._id}`)
+        .emit("receive_response_to_friend_request", data);
+      socket
+        .to(`user_${data.user2._id}`)
+        .emit("receive_response_to_friend_request", data);
+    }
   });
 
   socket.on("disconnect", () => {
