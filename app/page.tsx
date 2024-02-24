8c86200c5b2562a116fbac360b9c67bc0ebf6f6f
LevaniIlashvili <levaniko900@gmail.com>
2024-02-24 15:38:28 +0400

    minor bug change

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,6 @@ import { redirect } from "next/navigation";
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
-  if (!session) {
-    redirect("/login");
-  }
-
   if (session.user) {
     redirect("/dashboard");
   } else {
