8cf93677734d8925eebe0ac9d058bdd7c03552c6
LevaniIlashvili <levaniko900@gmail.com>
2024-02-25 15:06:19 +0400

    .

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,11 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-
-  console.log(session);
-
-  if (session?.user) {
-    redirect("/dashboard");
-  } else {
-    redirect("/login");
-  }
+  // const session = await getServerSession(authOptions);
+  // console.log(session);
+  // if (session?.user) {
+  //   redirect("/dashboard");
+  // } else {
+  //   redirect("/login");
+  // }
 }
