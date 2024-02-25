af0f7012017705ce0fcf2a408bb9c49d25f95d02
LevaniIlashvili <levaniko900@gmail.com>
2024-02-25 15:19:23 +0400

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-import { authOptions } from "@/authOptions";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
+// import { authOptions } from "@/authOptions";
+// import { getServerSession } from "next-auth";
+// import { redirect } from "next/navigation";
 
 export default async function Home() {
+  console.log("Hello");
   // const session = await getServerSession(authOptions);
   // console.log(session);
   // if (session?.user) {
@@ -10,4 +11,6 @@ export default async function Home() {
   // } else {
   //   redirect("/login");
   // }
+
+  return <div>hello</div>;
 }
