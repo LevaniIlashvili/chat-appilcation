4721e7d2f46cfb0bcc23ea631200142687c0e814
LevaniIlashvili <levaniko900@gmail.com>
2024-02-25 15:51:44 +0400

    uncomment content

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
-// import { authOptions } from "@/authOptions";
-// import { getServerSession } from "next-auth";
-// import { redirect } from "next/navigation";
+import { authOptions } from "@/authOptions";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-  console.log("Hello");
-  // const session = await getServerSession(authOptions);
-  // console.log(session);
-  // if (session?.user) {
-  //   redirect("/dashboard");
-  // } else {
-  //   redirect("/login");
-  // }
+  const session = await getServerSession(authOptions);
 
-  return <div>hello</div>;
+  if (session?.user) {
+    redirect("/dashboard");
+  } else {
+    redirect("/login");
+  }
 }
