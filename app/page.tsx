fa20275b53b3df1074a38a87a06a2bfde4b94896
LevaniIlashvili <levaniko900@gmail.com>
2024-03-09 11:44:08 +0400

    .

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
+  console.log("getting session");
   const session = await getServerSession(authOptions);
+
+  console.log("session", session);
 
   if (session?.user) {
     redirect("/dashboard");
